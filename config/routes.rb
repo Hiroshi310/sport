199ba6d1e928f84827135fa3b450a1c751c9e072
hiroshi310 <hiro560428@gmail.com>
2016-03-01 00:40:01 +0000

    herokunew

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :events
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +64,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
