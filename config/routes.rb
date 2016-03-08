da5bdeb55d2b57b5acba27f6ec24276b754a2228
hiroshi310 <hiro560428@gmail.com>
2016-03-08 15:55:25 +0000

    awesome_nested_set

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :events
+  resources :areas
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
