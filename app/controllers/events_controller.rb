e0603988d911a3ab9f13f0de2a559cf3c4da5aec
hiroshi310 <hiro560428@gmail.com>
2016-03-15 02:17:23 +0000

    areacustom2 firstcommit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,22 +3,6 @@ class EventsController < ApplicationController
 
   def new
     @event = current_user.events.build if logged_in?
-    @areas = Area.order(:parent_id)
-    
-    #都道府県　area.parent_id=nilのものを親(@areas_parent)としておく。
-    #子供(@areas_children)はそれ以外の時
-    @areas_parent = []
-    @areas_children = []
-    
-    @areas.each do |area|
-      if !!area.parent_id
-        @areas_children << area
-        # Areaを探してnullの場合@areas_children[parant_id.each]
-      else
-        @areas_parent << area
-      end
-    end
-
   end
 
   def create
